f4025f9e02bad8f4da1db0844cfdc872194c7500
michael <maroc.michael@hotmail.com>
2024-01-15 01:29:09 +0100

    ajout des composants login, layout et products + router et services

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,12 +1,99 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProductService } from '../../../services/product/product.service';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  isSidePanelVisible: boolean = false;
+  productObj: any = {
+    "productId": 0,
+    "productSku": "",
+    "productName": "",
+    "productPrice": 0,
+    "productShortName": "",
+    "productDescription": "",
+    "createdDate": new Date(),
+    "deliveryTimeSpan": "",
+    "categoryId": 0,
+    "productImageUrl": ""
+  };
+  categoryList: any[] = [];
+  productsList: any[] = [];
 
+  constructor(private productSrv: ProductService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getProducts();
+    this.getAllCategory();
+  }
+
+  getProducts() {
+    this.productSrv.getProducts().subscribe((res:any) => {
+      this.productsList = res.data;
+    })
+  }
+
+  getAllCategory() {
+    this.productSrv.getCategory().subscribe((res:any) => {
+      this.categoryList = res.data;
+    })
+  }
+
+  onUpdate() {
+    this.productSrv.updateProduct(this.productObj).subscribe((res:any) => {
+      if (res.result) {
+        alert('Product Edited');
+        this.getProducts();
+      } else (
+        alert(res.message)
+      )
+    })
+  }
+
+  onSave() {
+    this.productSrv.saveProduct(this.productObj).subscribe((res:any) => {
+      debugger;
+      if (res.result) {
+        alert('Product Created');
+        this.getProducts();
+      } else (
+        alert(res.message)
+      )
+    })
+  }
+
+  onDelete(item: any) {
+    const isDelete = confirm('Are you sure you want to delete ?');
+    this.productSrv.deleteProduct(item.productId).subscribe((res:any) => {
+      debugger;
+      if (isDelete) {
+        alert('Product Deleted');
+        this.getProducts();
+      } else (
+        alert(res.message)
+      )
+    })
+  }
+
+  onEdit(item: any) {
+    this.productObj = item;
+    this.openSidePanel();
+  }
+  
+  openSidePanel() {
+    this.isSidePanelVisible = true
+  }
+
+  closeSidePanel() {
+    this.isSidePanelVisible = false
+  }
 }
